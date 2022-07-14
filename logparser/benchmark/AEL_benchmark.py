d4468675d7af554eb5be99baa1fa3cea4f5f500e
Vithor Bertalan <vbertalan@gmail.com>
2022-07-14 12:43:22 -0300

    corrigido AEL

--- logparser/benchmark/AEL_benchmark.py
+++ logparser/benchmark/AEL_benchmark.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
 import sys
-sys.path.append('../')
-from logparser import AEL, evaluator
+sys.path.append("C:/Users/vbert/OneDrive/DOUTORADO Poly Mtl/Projeto/pyteste")
+from logparser.logparser.utils import evaluator
+from logparser.logparser.AEL import AEL
 import os
 import pandas as pd
+from pathlib import Path
 
-
-input_dir = '../logs/' # The input directory of log file
-output_dir = 'AEL_result/' # The output directory of parsing results
+input_dir = "C:/Users/vbert/OneDrive/DOUTORADO Poly Mtl/Projeto/pyteste/logparser/logs"  # The input directory of log file
+output_dir = 'logparser/results/AEL_result/'  # The output directory of parsing results
 
 benchmark_settings = {
     'HDFS': {
@@ -141,7 +142,7 @@ benchmark_settings = {
 }
 
 bechmark_result = []
-for dataset, setting in benchmark_settings.iteritems():
+for dataset, setting in benchmark_settings.items():
     print('\n=== Evaluation on %s ==='%dataset)
     indir = os.path.join(input_dir, os.path.dirname(setting['log_file']))
     log_file = os.path.basename(setting['log_file'])
@@ -161,4 +162,6 @@ print('\n=== Overall evaluation results ===')
 df_result = pd.DataFrame(bechmark_result, columns=['Dataset', 'F1_measure', 'Accuracy'])
 df_result.set_index('Dataset', inplace=True)
 print(df_result)
-df_result.T.to_csv('AEL_bechmark_result.csv')
+filepath = Path('logparser/results/AEL_result/AEL_bechmark_result.csv') 
+#df_result.T.to_csv('Drain_bechmark_result.csv')
+df_result.T.to_csv(filepath)
