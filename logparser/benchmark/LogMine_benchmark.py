343eefb13278bef7bb5ecdae15fc3239378594e1
Vithor Bertalan <vbertalan@gmail.com>
2022-07-15 16:50:25 -0300

    ERRO LogMine precisa de "water" python

--- logparser/benchmark/LogMine_benchmark.py
+++ logparser/benchmark/LogMine_benchmark.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
 import sys
-sys.path.append('../')
-from logparser import LogMine, evaluator
+sys.path.append("C:/Users/vbert/OneDrive/DOUTORADO Poly Mtl/Projeto/pyteste")
+from logparser.logparser.utils import evaluator
+from logparser.logparser.LogMine import LogMine
 import os
 import pandas as pd
+from pathlib import Path
 
-input_dir = '../logs/' # The input directory of log file
-output_dir = 'LogMine_result/' # The output directory of parsing results
+input_dir = "C:/Users/vbert/OneDrive/DOUTORADO Poly Mtl/Projeto/pyteste/logparser/logs"
+output_dir = 'logparser/results/LogMine_result/'  # The output directory of parsing results
 
 benchmark_settings = {
     'HDFS': {
@@ -156,7 +158,7 @@ benchmark_settings = {
 }
 
 bechmark_result = []
-for dataset, setting in benchmark_settings.iteritems():
+for dataset, setting in benchmark_settings.items():
     print('\n=== Evaluation on %s ==='%dataset)
     indir = os.path.join(input_dir, os.path.dirname(setting['log_file']))
     log_file = os.path.basename(setting['log_file'])
@@ -177,4 +179,6 @@ print('\n=== Overall evaluation results ===')
 df_result = pd.DataFrame(bechmark_result, columns=['Dataset', 'F1_measure', 'Accuracy'])
 df_result.set_index('Dataset', inplace=True)
 print(df_result)
-df_result.T.to_csv('LogMine_bechmark_result.csv')
+#df_result.T.to_csv('LogMine_bechmark_result.csv')
+filepath = Path('logparser/results/LogMine_result/LogMine_bechmark_result.csv') 
+df_result.T.to_csv(filepath)
