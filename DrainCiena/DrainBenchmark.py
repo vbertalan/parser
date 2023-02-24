a34f1743ce593e85c680ffe8f97a1b00d615e2b7
vbertalan <vbertalan@gmail.com>
2023-02-24 13:11:05 -0500

    ciena parser working

--- DrainCiena/DrainBenchmark.py
+++ DrainCiena/DrainBenchmark.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 
+from pathlib import Path
+import DrainCiena
 import sys
+import os
+
 # Path - Windows
 #sys.path.append("C:/Users/vbert/OneDrive/DOUTORADO Poly Mtl/Projeto/pyteste")
 # Path - Linux
 sys.path.append("/home/vbertalan/Downloads/Parser/parser/DrainCiena")
-import evaluator
-import DrainCiena
-import os
-import pandas as pd
-from pathlib import Path
 
 input_dir = "DrainCiena/logs/"
 output_dir = "DrainCiena/result/"  # The output directory of parsing results
 
-benchmark_settings = {
+log_datafiles = {
     'HDFS': {
         # Name of the log file
         'log_file': 'HDFS_2k.log',
@@ -39,27 +38,14 @@ benchmark_settings = {
         },
 }
 
-bechmark_result = []
-for dataset, setting in benchmark_settings.items():
+for dataset, setting in log_datafiles.items():
     print('\n=== Evaluation on %s ==='%dataset)
     indir = os.path.join(input_dir, os.path.dirname(setting['log_file']))
     log_file = os.path.basename(setting['log_file'])
 
     parser = DrainCiena.LogParser(log_format=setting['log_format'], indir=indir, outdir=output_dir, rex=setting['regex'], depth=setting['depth'], st=setting['st'])
     parser.parse(log_file)
+
+    parsedresult=os.path.join(output_dir, log_file + '_structured.csv')   
     
-    F1_measure, accuracy = evaluator.evaluate(
-                           groundtruth=os.path.join(indir, log_file + '_structured.csv'),
-                           parsedresult=os.path.join(output_dir, log_file + '_structured.csv')
-                           )
-    bechmark_result.append([dataset, F1_measure, accuracy])
-
-
-print('\n=== Overall evaluation results ===')
-df_result = pd.DataFrame(bechmark_result, columns=['Dataset', 'F1_measure', 'Accuracy'])
-df_result.set_index('Dataset', inplace=True)
-print(df_result)
-filepath = Path('Drain_bechmark_result.csv') 
-#df_result.T.to_csv('Drain_bechmark_result.csv')
-df_result.T.to_csv(filepath)
-
+print('\n=== Parsing finished ===')
