b9d668ffbb6bbd6125455d8ed1ffeb4871e63019
vbertalan <vbertalan@gmail.com>
2023-04-20 12:02:29 -0700

    correcoes ubuntu

---
+++ python-job-ubuntu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --mem=32G
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:p100:1
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=15:0:0    
+#SBATCH --mail-user=<vbertalan@gmail.com>
+#SBATCH --mail-type=ALL
+
+cd $projects/Parser-CC/parser
+source .venv/bin/activate
+
+python transformer-trainer.py
