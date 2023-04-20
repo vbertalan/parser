6deff65f1ceb0907e284ef1ae317d68beab0c0c5
vbertalan <vbertalan@gmail.com>
2023-04-20 14:36:27 -0400

    alterando trainer

--- transformer-trainer.py
+++ transformer-trainer.py
@@ -11,7 +11,7 @@ from pathlib import Path
 import os
 
 #os.chdir("/home/vbertalan/Downloads/Raw Logs/")
-path = "Parser/logs/Hadoop/Hadoop_2k.log"
+path = "Parser/logs/Hadoop/Hadoop-full.log"
 
 ## Check if there is a GPU
 #!nvidia-smi
